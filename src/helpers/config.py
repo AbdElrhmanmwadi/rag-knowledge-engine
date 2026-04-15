86d5d807833df0ad9fa05b58265de2a4a254fbb3
AbdelRahman wadi <127618783+AbdElrhmanmwadi@users.noreply.github.com>
2026-04-15 14:47:46 +0300

    Add PostgreSQL configuration to .env.example and update Settings class

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -7,6 +7,11 @@ class Settings(BaseSettings):
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
+    POSTGRES_USERNAME: str
+    POSTGRES_PASSWORD: str  
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
 
     class Config:
         env_file = ".env"
