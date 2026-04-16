4d642d01169b2aecea69c2a95277e2c12c25ea71
AbdelRahman wadi <127618783+AbdElrhmanmwadi@users.noreply.github.com>
2026-04-16 17:43:36 +0300

    implement project model

---
+++ src/models/ProjectModel.py
@@ -0,0 +1,60 @@
+from sqlalchemy import func, select
+
+from models.db_schemes.minirag.scheme import Project
+
+from .BaseDataModle import BaseDataModel
+from .enums.DatabaseEunm import DataBaseEnum
+class ProjectModel(BaseDataModel):
+    def __init__(self, db_client: object):
+        super().__init__(db_client)
+        self.db_client=db_client
+
+    @classmethod
+    async def create_instance(cls,db_client)  :
+        instance= cls(db_client)
+     #   await instance.init_collection()
+        return instance
+
+    async def create_project(self, project: Project):
+        async with self.db_client() as session:
+            session.add(project)
+            await session.commit()
+            await session.refresh(project)
+            return project
+        
+    async def get_project_or_create(self, project_id: str):
+        try:
+            project_id_int = int(project_id)
+        except (TypeError, ValueError) as e:
+            raise ValueError(f"project_id must be an integer string, got: {project_id!r}") from e
+
+        async with self.db_client() as session:
+            query = select(Project).where(Project.project_id == project_id_int)
+            result = await session.execute(query)
+
+            project_obj = result.scalar_one_or_none()
+
+            if project_obj is None:
+                new_project = Project(project_id=project_id_int)
+                session.add(new_project)
+                await session.commit()
+                await session.refresh(new_project)
+                return new_project
+
+            return project_obj
+        
+    async def get_all_projects(self,page:int=1,page_size:int=10):
+        async with self.db_client() as session:
+            async with session.begin():
+                total_query=select(func.count(Project.project_id))
+                total_result= await session.execute(total_query)
+                total_documents=total_result.scalar_one()
+                total_page=total_documents// page_size
+                if total_documents% page_size>0:
+                    total_page +=1
+                query=select(Project).offset((page-1)*page_size).limit(page_size)
+                result = await session.execute(query)
+                projects = result.scalars().all()
+                return projects,total_page
+            
+        
