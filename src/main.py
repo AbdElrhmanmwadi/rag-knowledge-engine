2707ba23a23f474770b431152700604108dd4e84
AbdelRahman wadi <127618783+AbdElrhmanmwadi@users.noreply.github.com>
2026-04-18 13:19:34 +0300

    Add database models for Asset, DataChunk, and Project; implement startup event for async PostgreSQL connection

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
 from routes import base, data
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from helpers.config import get_settings
 app = FastAPI()
+@app.on_event("startup")
+async def startup_event():
+    settings = get_settings()
+    postgres_conn = f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+    app.db_engine = create_async_engine(postgres_conn)
+    app.db_client = sessionmaker(app.db_engine, expire_on_commit=False, class_=AsyncSession)
+
+
+async def shutdown_event():
+    await app.db_engine.dispose()
+
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
