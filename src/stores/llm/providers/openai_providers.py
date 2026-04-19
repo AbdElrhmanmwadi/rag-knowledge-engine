d0746863a578fe19a43cf50117a2ade6d158cde9
AbdelRahman wadi <127618783+AbdElrhmanmwadi@users.noreply.github.com>
2026-04-19 17:37:41 +0300

    Add OpenAI provider implementation and update requirements

---
+++ src/stores/llm/providers/openai_providers.py
@@ -0,0 +1,82 @@
+from  LLMinterface import LLMInterface
+from LLMEnum import OpenAIEnums
+from openai import OpenAI
+import logging
+class OpenAIprovider(LLMInterface):
+    def __init__(self,api_key:str,api_url:str=None,
+                 default_input_max_caracters:int=1000,
+                 default_output_max_caracters:int=1000,
+                 default_temperature:float=0.1,
+                 ):
+        self.api_key=api_key
+        self.api_url=api_url
+        self.default_input_max_caracters=default_input_max_caracters
+        self.default_output_max_caracters=default_output_max_caracters
+        self.default_temperature=default_temperature
+        self.genaration_model_id=None
+        self.embedding_model_id=None
+        self.embedding_size=None
+        self.client=OpenAI(api_key=self.api_key,
+                           api_url=self.api_url)
+        self.logger = logging.getLogger(__name__)
+        
+
+        super().__init__()
+        self.llm=OpenAI()
+
+    def set_genaration_model(self, model_id:str):
+        self.genaration_model_id=model_id
+
+    def set_embedding_model(self, model_id:str,embedding_size:int):
+        self.embedding_model_id=model_id
+        self.embedding_size=embedding_size
+
+    def process_text(self,text:str):
+        return text[:self.default_input_max_caracters].strip()
+
+
+    def genarate_text(self, prompt:str,max_output_tokens:int=None,chat_hestory:list=[],temperature:float=None):
+        if not self.client:
+            self.logger.error("OpenAI CLIENT was not set ")
+            return None
+        if not self.generation_model_id:
+            self.logger.error("generation model for OpenAI was not set ")
+            return None
+        max_output_tokens=max_output_tokens if max_output_tokens else self.default_output_max_caracters
+        temperature=temperature if temperature  else self.default_temperature
+        chat_hestory.append(self.constract_prompt(prompt=prompt,role=OpenAIEnums.USER.value))
+        response=self.client.chat.completions.create(
+            model=self.genaration_model_id,
+            messages=chat_hestory,
+            max_tokens=max_output_tokens,
+            temperature=temperature
+        )
+        if not response or not response.choices or len(response.choices) == 0 or not response.choices[0].message:
+            self.logger.error("Error while generating text with openai")
+            return None
+        return response.choices[0].message
+
+
+        
+
+    def embed_text(self, text:str,documant_type:str=None):
+        if not self.client:
+            self.logger.error("OpenAI CLIENT was not set ")
+            return None
+        if not self.embedding_model_id:
+            self.logger.error("embedding model for OpenAI was not set ")
+            return None
+        response =self.client.embeddings.create(
+            model=self.embedding_model_id,
+            input=text
+        )
+        if response is None or response.data is None or len(response.data) == 0:
+            self.logger.error("Error while embedding text with openai")
+            return None
+        return response.data[0].embedding
+    def constract_prompt(self, prompt:str,role:str):
+        return{
+            "role":role,
+            "content":self.process_text(text=prompt)
+        }
+
