8301fbc02a4350cd5b86d9d5a7e9c61fae3ab00e
AbdelRahman wadi <127618783+AbdElrhmanmwadi@users.noreply.github.com>
2026-04-21 13:44:56 +0300

    Add NLP router and initialize LLM and VectorDB providers in startup event

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-from routes import base, data
+from routes import base, data,nlp
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from helpers.config import get_settings
+from stores.llm.providers.LLMProviderFactory import LLMProviderFactory
+
+from stores.Vectordb.VectorDBProviderFactory import VectorDBProviderFactory   
 app = FastAPI()
 @app.on_event("startup")
 async def startup_event():
@@ -10,10 +13,28 @@ async def startup_event():
     postgres_conn = f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
     app.db_engine = create_async_engine(postgres_conn)
     app.db_client = sessionmaker(app.db_engine, expire_on_commit=False, class_=AsyncSession)
+    LLM_Provider_Factory=LLMProviderFactory(settings)
+    vectordb_provider_factory = VectorDBProviderFactory(settings)
+
+
+
+    app.generation_client = LLM_Provider_Factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
+
+    app.embedding_client = LLM_Provider_Factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
+    app.vectordb_client = vectordb_provider_factory.create(provider=settings.VECTOR_DB_BACKEND)
+    app.vectordb_client.connect()
+
+    
+        
 
 @app.on_event("shutdown")
 async def shutdown_event():
     await app.db_engine.dispose()
+    app.vectordb_client.disconnect()
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
