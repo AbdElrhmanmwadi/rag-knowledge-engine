8d49628c0aad0b9a9796d49f3bd02449fdab1db0
AbdelRahman wadi <127618783+AbdElrhmanmwadi@users.noreply.github.com>
2026-04-26 12:35:04 +0300

    Refactor vector database provider initialization and add error handling for unsupported providers

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ async def startup_event():
     app.db_engine = create_async_engine(postgres_conn)
     app.db_client = sessionmaker(app.db_engine, expire_on_commit=False, class_=AsyncSession)
     LLM_Provider_Factory=LLMProviderFactory(settings)
-    vectordb_provider_factory = VectorDBProviderFactory(settings)
+    vectordb_provider_factory = VectorDBProviderFactory(config=settings,db_client=app.db_client)
     
 
 
@@ -27,8 +27,12 @@ async def startup_event():
     app.embedding_client = LLM_Provider_Factory.create(provider=settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
                                              embedding_size=settings.EMBEDDING_MODEL_SIZE)
+    
     app.vectordb_client = vectordb_provider_factory.create(provider=settings.VECTOR_DB_BACKEND)
-    app.vectordb_client.connect()
+    if app.vectordb_client is None:
+        raise ValueError(f"Failed to create vector database client. Unsupported provider: {settings.VECTOR_DB_BACKEND}. Supported providers: QDRANT, PGVECTOR")
+    
+    await app.vectordb_client.connect()
     app.template_parser= TemplateParser(
         language=settings.PRIMARY_LANG,
         defult_language=settings.DEFAULT_LANG
@@ -41,7 +45,7 @@ async def startup_event():
 @app.on_event("shutdown")
 async def shutdown_event():
     await app.db_engine.dispose()
-    app.vectordb_client.disconnect()
+    await app.vectordb_client.disconnect()
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
