7f17459321a69b3f640bc505f1e32b203924eeba
AbdelRahman wadi <127618783+AbdElrhmanmwadi@users.noreply.github.com>
2026-04-26 12:29:29 +0300

    Refactor index_project function to enhance vector database indexing with progress tracking and async operations

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -8,13 +8,15 @@ from models.enums.ResponseEnums import ResponseStatus
 from routes.schemes.nlp import PushRequest,searchRequest
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunkModel
+from tqdm.auto import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
 
 nlp_router=APIRouter(
     prefix="/api/v1/nlp",
     tags=["api-v1","nlp"]
 )
 @nlp_router.post("/index/push/{project_id}")
-async def get_project_index_info(request:Request,project_id:int,Push_Request:PushRequest):
+async def index_project(request:Request,project_id:int,Push_Request:PushRequest):
     project_model = await ProjectModel.create_instance(
         db_client=request.app.db_client
     )
@@ -41,24 +43,50 @@ async def get_project_index_info(request:Request,project_id:int,Push_Request:Pus
     has_record=True
     page_no=1
     inserted_items_count=0
-    while has_record:
-        page_chunks=await chunk_model.get_chunks_by_project_id(
-        project_id=project.project_id,page_number=page_no)
-        is_first_page = (page_no == 1)
-        if not page_chunks or len(page_chunks)==0:
-            has_record=False
-            break
-        is_inserted = nlp_controller.index_into_vectordb(project=project,chunks=page_chunks,do_reset=is_first_page)
-        if not is_inserted:
-            return JSONResponse(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                content={
-                    "signal": ResponseStatus.INSERT_INTO_VECTORDB_ERROR.value
-                }
-            )
-        inserted_items_count +=len(page_chunks)
-        page_no += 1 
 
+    _=await request.app.vectordb_client.create_collection(
+        collection_name=nlp_controller.create_collection_name(project_id=project.project_id),
+        embedding_size=request.app.embedding_client.embedding_size,
+        do_reset=Push_Request.do_reset) 
+    
+
+    #batch
+    total_chunks_count= await chunk_model.get_chunks_count_by_project_id(project_id=project.project_id)
+    bar=tqdm(total=total_chunks_count,desc="Indexing chunks into vector database",position=0)
+
+
+    if total_chunks_count==0:
+        has_record=False
+    
+
+
+
+
+    with logging_redirect_tqdm():
+        while has_record:
+            page_chunks=await chunk_model.get_chunks_by_project_id(
+            project_id=project.project_id,page_number=page_no)
+            is_first_page = (page_no == 1)
+            if not page_chunks or len(page_chunks)==0:
+                has_record=False
+                break
+            is_inserted =await nlp_controller.index_into_vectordb(project=project,chunks=page_chunks,do_reset=is_first_page)
+            if not is_inserted:
+                return JSONResponse(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content={
+                        "signal": ResponseStatus.INSERT_INTO_VECTORDB_ERROR.value
+                    }
+                )
+            bar.update(len(page_chunks))
+            inserted_items_count +=len(page_chunks)
+            page_no += 1 
+
+        # Create vector index once after all data is inserted
+        collection_name = nlp_controller.create_collection_name(project_id=project.project_id)
+        await request.app.vectordb_client.create_vector_index(collection_name=collection_name)
+        bar.close()
+    
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content={
@@ -66,7 +94,7 @@ async def get_project_index_info(request:Request,project_id:int,Push_Request:Pus
         }
     )
 @nlp_router.get("/index/info/{project_id}")
-async def get_project_index_infoo(request:Request,project_id:int):
+async def get_project_index_info(request:Request,project_id:int):
     project_model = await ProjectModel.create_instance(
         db_client=request.app.db_client
     )
@@ -87,7 +115,7 @@ async def get_project_index_infoo(request:Request,project_id:int):
         generation_client=request.app.generation_client,
         template_parser=request.app.template_parser
     )
-    collection_info= nlp_controller.get_vectordb_collection_info(project=project)
+    collection_info= await nlp_controller.get_vectordb_collection_info(project=project)
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content={
@@ -117,7 +145,7 @@ async def search_project_index(request:Request,project_id:int,search_request:sea
         generation_client=request.app.generation_client,
         template_parser=request.app.template_parser
     )
-    search_result= nlp_controller.search_in_vectordb(project=project,text=search_request.text,limit=search_request.limit)
+    search_result=await nlp_controller.search_in_vectordb(project=project,text=search_request.text,limit=search_request.limit)
     if search_result is False:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -161,7 +189,7 @@ async def answer_rag_question(request:Request,project_id:int,search_request:sear
         template_parser=request.app.template_parser
     )
     
-    rag_answer,full_prompt,chat_history= nlp_controller.answer_rag_question(query=search_request.text,project=project,limit=search_request.limit)
+    rag_answer,full_prompt,chat_history= await nlp_controller.answer_rag_question(query=search_request.text,project=project,limit=search_request.limit)
     if not rag_answer:
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,content={"signal":ResponseStatus.RAG_ANSWER_FAILED.value,"message":"No answer could be generated from the retrieved documents"})
     return JSONResponse(status_code=status.HTTP_200_OK,content={"signal":ResponseStatus.RAG_ANSWER_SUCCESS.value,
